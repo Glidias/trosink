8e8f865d3cb16980bece4aa4d418c350788ed3bd
Glenn Ko <glidias@hotmail.com>
2017-10-11 00:50:42 +0800

    fix recoveryrate

--- haxe/src/troshx/sos/bnb/Fat.hx
+++ haxe/src/troshx/sos/bnb/Fat.hx
@@ -2,6 +2,7 @@ package troshx.sos.bnb;
 import troshx.sos.core.BoonBane.Bane;
 import troshx.sos.core.Modifier;
 import troshx.sos.core.Modifier.StaticModifier;
+import troshx.sos.macro.MacroUtil;
 
 /**
  * ...
@@ -10,11 +11,13 @@ import troshx.sos.core.Modifier.StaticModifier;
 class Fat extends Bane {
 	public function new() {
 		super("Fat", [5]);
-		// -2 to endurance when determining fatque
-		
-		staticModifiers = [
+		var m; 
+		MacroUtil.linkedListFromArray(m, [
 			StaticModifier.create(Modifier.CMP_MOB, -2),
-			StaticModifier.create(Modifier.FATIQUE_END, -2),
-		];
+			StaticModifier.create(Modifier.FATIQUE_END, -2)
+		]);
+		staticModifiers = [m];
+		
+		
 	}
 }
--- haxe/src/troshx/sos/bnb/Skinny.hx
+++ haxe/src/troshx/sos/bnb/Skinny.hx
@@ -2,6 +2,7 @@ package troshx.sos.bnb;
 import troshx.sos.core.BoonBane.Bane;
 import troshx.sos.core.Modifier;
 import troshx.sos.core.Modifier.SituationalCharModifier;
+import troshx.sos.macro.MacroUtil;
 import troshx.sos.sheets.CharSheet;
 /**
  * ...
@@ -11,10 +12,11 @@ class Skinny extends Bane {
 	public function new() {
 		
 		super("Skinny", [3]);
-		staticModifiers = [
+		var m; 
+		MacroUtil.linkedListFromArray(m, [
 			StaticModifier.create(Modifier.CAR_END, -1)
-			//StaticModifier.create(Modifier.FATIQUE_END, -1)
-		];
+		]);
+		staticModifiers = [m];
 	}
 }
 
