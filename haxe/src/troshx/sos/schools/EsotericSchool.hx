04eb6edbef29ac9c626d3ccd3ef863a891ff6c6b
Glidias <glidias@hotmail.com>
2019-05-08 13:19:48 +0800

    esoteric school fix benefit choices.

--- haxe/src/troshx/sos/schools/EsotericSchool.hx
+++ haxe/src/troshx/sos/schools/EsotericSchool.hx
@@ -41,7 +41,7 @@ class EsotericSchoolBonuses extends SchoolBonuses {
 	@:flag public static inline var BITING_WIND:Int = (1 << 3);
 	@:flag public static inline var STOIC_EARTH:Int = (1 << 4);
 	
-	@:ui({type:"SingleSelection", label:"Choose Benefit(s)", labels:["Simplicity, Flowing Water", "Raging Flame", "Biting Wind", "Stoic Earth"], values:[SIMPLICITY|FLOWING_WATER, RAGING_FLAME, BITING_WIND, STOIC_EARTH] }) public var flags:Int = SIMPLICITY|FLOWING_WATER;
+	@:ui({type:"SingleSelection", label:"Choose Element", labels:["Flowing Water", "Raging Flame", "Biting Wind", "Stoic Earth"], values:[SIMPLICITY|FLOWING_WATER, SIMPLICITY|RAGING_FLAME, SIMPLICITY|BITING_WIND, SIMPLICITY|STOIC_EARTH] }) public var flags:Int = SIMPLICITY|FLOWING_WATER;
 	
 	public function new() {
 		super();
