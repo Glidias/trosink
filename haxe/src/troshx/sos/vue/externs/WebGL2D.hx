f22e2bb364d00ea1a6a296ee6cecb8c76c7535f7
Glidias <glidias@hotmail.com>
2019-07-01 11:06:05 +0800

    externs webgl2d

---
+++ haxe/src/troshx/sos/vue/externs/WebGL2D.hx
@@ -0,0 +1,14 @@
+package troshx.sos.vue.externs;
+import js.html.CanvasElement;
+
+/**
+ * ...
+ * @author Glidias
+ */
+@:native("WebGL2D") 
+extern class WebGL2D 
+{
+
+	public static function enable(elem:CanvasElement):Void;
+	
+}
